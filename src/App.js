01f35de8b4366ed71899b793d30d24376d1caa13
Vicente Albuquerque <7.vincent@gmail.com>
2019-12-23 18:02:55 -0300

    Configurado redux.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Routes from "./routes";
+import {Provider} from 'react-redux';
 
 import Header from './components/Header';
 
 import StyleGlobal from "./styles/global";
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes />
-      <StyleGlobal />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+        <StyleGlobal />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {darket, darken} from 'polished';
+import { darken} from 'polished';
 export const Container = styled.div`
   padding: 30px;
   background: #fff;
