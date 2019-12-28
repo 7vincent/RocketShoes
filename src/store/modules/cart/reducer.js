8d86feb3b85c551b4fa8894a1885c6028c633eec
Vicente Albuquerque <7.vincent@gmail.com>
2019-12-27 22:04:55 -0300

    Retirando produto duplicado no cart, somando amount.

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,10 +1,30 @@
+import produce from 'immer';
+
 export default function cart(state =[], action){
   switch(action.type){
     case 'ADD_TO_CART':
-      return [...state, 
+            //(estado atual, copia do estado)
+      return produce(state, draft=>{
+        //pegando o id do produto que chegou aqui
+        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        
+        //verificando se o produto já existe no carrinho, se existir, modifico apenas a quantidade
+        if(productIndex >= 0){
+          draft[productIndex].amount += 1;
+        }else{
+          draft.push({
+            ...action.product,
+            amount: 1,
+          });
+        }  
+      });
+ /*
+ Sem o immer 
+ return [...state, 
         {...action.product,
         amount: 1,
         }];
+*/
     default:
       return state;
   }
